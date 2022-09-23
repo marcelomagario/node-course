5ac667c34a1cfa3f45cfad8489633d1f397783c4
Marcelo <marcelomagario@gmail.com>
2022-09-23 21:05:19 +1000

    Installed NPM validator package

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,5 +1,8 @@
-const getNotes = require('./notes.js');
+const validator = require('validator')
+const getNotes = require('./notes.js')
 
-const msg = getNotes();
+const msg = getNotes()
 
-console.log(msg);
+console.log(msg)
+console.log(validator.isEmail('marcelo@gmail.com'))
+console.log(validator.isURL('https://www.marcelo.com'))
